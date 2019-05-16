420f82db59835c100d1b661c02672dbfea30bc94
Maw Ceron <ceron.maw@gmail.com>
2019-05-16 17:07:12 -0500

    Adds GetGoogleTranslation function

--- Tets/Classes.cs
+++ Tets/Classes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -135,6 +136,38 @@ namespace Tets
                 return false;
 
             return true;
+        }
+
+        public static string GetGoogleTranslation(string dialog,string langFrom, string langTo)
+        {
+            string data = String.Empty;
+
+            string urlAddress = String.Format(@"http://translate.google.com/m?hl={0}&sl={1}&q={2}",langTo,langFrom,dialog);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                if (response.CharacterSet == null)
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                data = readStream.ReadToEnd();
+
+                response.Close();
+                readStream.Close();
+            }
+           
+            return data;
         }
     }
 }
