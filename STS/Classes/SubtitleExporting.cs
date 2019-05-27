bf5b06e43d823f4735291b4e7e357e5007f82903
Maw Ceron <ceron.maw@gmail.com>
2019-05-27 17:15:56 -0500

    Unnecesary using removed

--- STS/Classes/SubtitleExporting.cs
+++ STS/Classes/SubtitleExporting.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace STS
 {
